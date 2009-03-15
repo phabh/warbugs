ae3e5f793ce6594db3c00420cbdc5bcb7dfab952
Paulo Abreu <phabh@users.noreply.github.com>
2009-03-15 12:54:16 +0000

    Primeira Versão do servidor, funcionando apenas a cmunicação com o banco de dados

---
+++ servidor/Projeto/WarBugs_Server/WarBugs_Server/WarBugs_Server.cpp
@@ -0,0 +1,16 @@
+// WarBugs_Server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Menu.h"
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Menu menu;
+	menu.criarMenuPrincipal();
+	return 0;
+}
+
