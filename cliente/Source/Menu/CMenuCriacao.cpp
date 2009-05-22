01d8b62d96d931ef47ed1e2afc1575ab24f1af33
fantini <fantini@a5a07eb8-a452-4904-a5b2-c285070d5910>
2009-05-22 15:18:20 +0000

    Reestruturação de classes

---
+++ cliente/Source/Menu/CMenuCriacao.cpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include "CMenu.h"
+
+class CMenuCriacao : public CMenu
+{
+
+private:
+
+	ISceneNode *_nodoSelecionado;
+	int _idPersonagem;
+
+	void graphicsDrawAddOn(){}
+
+	void updateHuds()
+	{
+
+		_gerenciadorHud->clear();
+		_gerenciadorHud->addButton(rect<s32>(440,500,540,540), 0, 7, L"Criar");
+
+		_flags[HUDCHANGED] = false;
+	}
+
+	void readCommands()
+	{
+		//_timer->update();
+
+		if(_gerenciadorEventos->isKeyPressed(KEY_ESCAPE))
+		{
+			_nextID = MN_SAIDA;
+		    return;
+		}
+ 
+		if(_gerenciadorEventos->isMouseButtonReleased(MBLEFT))
+		{
+			// Clique com o botao esquerdo
+						
+			if(_flags[OBJSELECTED])
+			{
+				_flags[OBJSELECTED] = false; // Drop 3D
+				_flags[HUDCHANGED] = true;
+			}
+			else
+			{
+				_idPersonagem = -1;
+				_nodoSelecionado = _gerenciadorCena->getSceneCollisionManager()->getSceneNodeFromScreenCoordinatesBB(_dispositivo->getCursorControl()->getPosition());
+
+				if (_nodoSelecionado)
+					_idPersonagem = _nodoSelecionado->getID();
+
+				if(_idPersonagem > 0)
+				{
+				   _flags[OBJSELECTED] = true; // Get 3D
+				   _flags[HUDCHANGED] = true;
+				}
+			}
+
+			if(_gerenciadorEventos->getEventCallerByElement(EGET_BUTTON_CLICKED))
+			{
+				// Trata os cliques em botões
+
+				switch (_gerenciadorEventos->getEventCallerByID())
+				{
+					case 7:
+						_nextID = MN_SELECAOPERSONAGEM;
+						return;
+					break;
+					
+					default:
+						cout << "\nID de botao desconhecido." << endl;
+				};
+			}		
+		}
+	}
+
+	void updateGraphics()
+	{
+		//_timer->update();
+	}
+	
+
+public:
+
+	CMenuCriacao(){}
+	
+	bool start(IrrlichtDevice *grafico, ISoundEngine *audio, CGameData *gameData)
+	{
+
+		_gameCfg = new CArquivoConfig();
+		TypeCfg cfg = _gameCfg->loadConfig();
+
+		_gameData = gameData;
+		
+		_dispositivo = grafico;
+		_gerenciadorEventos = (CGerEventos*)_dispositivo->getEventReceiver();
+		_gerenciadorAudio = audio;
+		_gerenciadorAudio->removeAllSoundSources();
+		
+		_myID = _nextID = MN_CRIACAOPERSONAGEM;
+		_arquivoCena = "recursos/cenas/criacao.irr";
+		//_timer = new CTimer();
+		//_timer->initialize();
+		_nodoSelecionado = 0;
+		_idPersonagem = -1;
+		_flags[OBJSELECTED] = false;
+		_flags[HUDCHANGED] = false;
+
+		_dispositivo->setWindowCaption(L"Warbugs - BETA Version 0.1");
+
+		_gerenciadorCena = _dispositivo->getSceneManager();   // Cria o gerenciador de cena
+		_gerenciadorVideo = _dispositivo->getVideoDriver();   // Cria o driver para o vídeo
+		_gerenciadorHud = _dispositivo->getGUIEnvironment(); // Cria o gerenciador de menu
+
+		_musica = _gerenciadorAudio->play2D("recursos/audio/criacao.ogg", true, false, false, ESM_AUTO_DETECT);
+		
+		_gerenciadorAudio->setSoundVolume(cfg.parametrosAudio.volumeMusica);
+
+		_gerenciadorCena->clear();
+		if(_arquivoCena)
+			_gerenciadorCena->loadScene(_arquivoCena);
+		
+		_skin = _gerenciadorHud->getSkin();
+		_font = _gerenciadorHud->getFont("recursos/fonts/font_georgia.png");
+		
+		if (_font)
+			_skin->setFont(_font);
+
+		_skin->setFont(_gerenciadorHud->getBuiltInFont(), EGDF_TOOLTIP);
+
+		
+		_camera = _gerenciadorCena->addCameraSceneNode(0,vector3df(0,50,0), vector3df(0,0,50));
+
+		return (true);
+	}
+};
---
+++ cliente/Source/Menu/CMenuLogin.cpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "CMenu.h"
+
+class CMenuLogin : public CMenu
+{
+
+private:
+
+	void graphicsDrawAddOn(){}
+
+	void updateHuds()
+	{
+		_gerenciadorHud->clear();
+		IGUIImage *img =_gerenciadorHud->addImage(rect<s32>(0, 0, _dispositivo->getVideoDriver()->getScreenSize().Width, _dispositivo->getVideoDriver()->getScreenSize().Height), 0, -1, L"");
+		img->setImage(_gerenciadorVideo->getTexture("recursos/texturas/huds/tx2d_bg_login.jpg"));
+		_gerenciadorHud->addEditBox(L"login", rect<s32>(300,500,400,520), true, 0, 10);
+		_gerenciadorHud->addEditBox(L"senha", rect<s32>(300,530,400,550), true, 0, 20);
+		_gerenciadorHud->addButton(rect<s32>(420,500,520,550), 0, 2, L"conectar");
+	}
+
+	void readCommands()
+	{
+		if(_gerenciadorEventos->isKeyDown(KEY_ESCAPE))
+		{
+			_nextID = MN_SAIDA;
+			return;
+		}
+
+		if(_gerenciadorEventos->getEventCallerByElement(EGET_BUTTON_CLICKED))
+		{
+			// Trata os cliques em botões
+
+			if (_gerenciadorEventos->getEventCallerByID() == 2)
+			{
+				// Clicou no botão conectar
+				_nextID = MN_SELECAOPERSONAGEM;
+				return;
+			}
+		}
+	}
+
+	void updateGraphics()
+	{
+	}
+	
+
+public:
+
+	CMenuLogin(){}
+	
+	bool start(IrrlichtDevice *grafico, ISoundEngine *audio, CGameData *gameData)
+	{
+		_gameCfg = new CArquivoConfig();
+		TypeCfg cfg = _gameCfg->loadConfig();
+
+		_gameData = gameData;
+
+	    _dispositivo = grafico;
+		_gerenciadorEventos = (CGerEventos*)_dispositivo->getEventReceiver();
+		_gerenciadorAudio = audio;
+		_gerenciadorAudio->removeAllSoundSources();
+
+		_myID = _nextID = MN_LOGIN;
+
+		_dispositivo->setWindowCaption(L"Warbugs - BETA Version 0.1");
+
+		_gerenciadorCena = _dispositivo->getSceneManager();   // Cria o gerenciador de cena
+		_gerenciadorVideo = _dispositivo->getVideoDriver();   // Cria o driver para o vídeo
+		_gerenciadorHud = _dispositivo->getGUIEnvironment(); // Cria o gerenciador de menu
+
+		_musica = _gerenciadorAudio->play2D("recursos/audio/login.ogg", true, false, false, ESM_AUTO_DETECT);
+		
+		_gerenciadorAudio->setSoundVolume(cfg.parametrosAudio.volumeMusica);
+
+		_gerenciadorCena->clear();
+
+		
+		_skin = _gerenciadorHud->getSkin();
+		_font = _gerenciadorHud->getFont("recursos/fonts/font_georgia.png");
+		
+		if (_font)
+			_skin->setFont(_font);
+
+		_skin->setFont(_gerenciadorHud->getBuiltInFont(), EGDF_TOOLTIP);
+		
+		_camera = _gerenciadorCena->addCameraSceneNode(0,vector3df(0,50,0), vector3df(0,0,50));
+
+		_flags[HUDCHANGED] = false;
+
+		return (true);
+	}
+};
