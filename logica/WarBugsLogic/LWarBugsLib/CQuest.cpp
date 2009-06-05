d82b1df4401e575bfa9ac8ea8c4d0b6275a14da6
ederfigueiredo <edragon975@gmail.com>
2009-06-05 05:57:24 +0000

    Implementação das quests.

--- logica/WarBugsLogic/LWarBugsLib/CQuest.cpp
+++ logica/WarBugsLogic/LWarBugsLib/CQuest.cpp
@@ -10,45 +10,66 @@
 #define _CQUEST
 
 #include "CQuest.h"
-
 CQuest::CQuest(CPersonagemJogador *jogador)
 {
 	setID(-1);
+	_estado = Q_CLOSED;
 	_jogador = jogador;
+	_contratante = NULL;
 	_itemNecessario = I_NONE;
 	_inimigoNecessario = M_NONE;
-	_qtdNecessaria = 0;
+	_qtdNecessariaItem = 0;
+	_qtdNecessariaInimigo = 0;
+	_monstrosDerrotados = 0;
+	_itensPossuidos = 0;
 	_recompensaItem = NULL;
 	_recompensaDinheiro = 0;
 	_recompensaXP = 0;
 	_recompensaLealdade = new CLealdade();
 }
+CQuest::CQuest(CPersonagemJogador *jogador, CPersonagem *npc, int itemID, int qtdItem, int monstID, int qtdMonst, int rewdXp, int rewdMny, CLealdade *rewdLdd, CItem *rewdItm)
+{
+	setID(-1);
+	_estado = Q_CLOSED;
+	_jogador = jogador;
+	_contratante = npc;
+	_itemNecessario = (TypeItens)itemID;
+	_inimigoNecessario = (TypeModelos)monstID;
+	_qtdNecessariaItem = qtdItem;
+	_qtdNecessariaInimigo = qtdMonst;
+	_monstrosDerrotados = 0;
+	_itensPossuidos = 0;
+	_recompensaItem = rewdItm;
+	_recompensaDinheiro = rewdMny;
+	_recompensaXP = rewdXp;
+	_recompensaLealdade = rewdLdd;
+}
+
+
 //Getters
 CPersonagemJogador *CQuest::getPlayer()
 {
 	return(_jogador);
 }
+CPersonagem *CQuest::getNPC()
+{
+	return(_contratante);
+}
 int CQuest::getRequestedItem()
 {
-	if(_itemNecessario != I_NONE)
-		return((int)_itemNecessario);
-	else if(_inimigoNecessario != M_NONE)
-		return(getRequestedEnemy());
-	else
-		return(0);
+	return((int)_itemNecessario);
 }
 int CQuest::getRequestedEnemy()
 {
-	if(_inimigoNecessario != M_NONE)
-		return((int)_inimigoNecessario);
-	else if(_itemNecessario != I_NONE)
-		return(getRequestedItem());
-	else
-		return(0);
+	return((int)_inimigoNecessario);
 }
-int CQuest::getRequestedNumber()
+int CQuest::getRequestedNumberOfItens()
 {
-	return(_qtdNecessaria);
+	return(_qtdNecessariaItem);
+}
+int CQuest::getRequestedNumberOfEnemies()
+{
+	return(_qtdNecessariaInimigo);
 }
 int CQuest::getReward()
 {
@@ -71,6 +92,10 @@ void CQuest::setPlayer(CPersonagemJogador *player)
 {
 	_jogador = player;
 }
+void CQuest::setNPC(CPersonagem *npc)
+{
+	_contratante = npc;
+}
 void CQuest::setRequestedItem(int reqItem)
 {
 	_itemNecessario = (TypeItens)reqItem;
@@ -79,9 +104,13 @@ void CQuest::setRequestedEnemy(int reqEnemy)
 {
 	_inimigoNecessario = (TypeModelos)reqEnemy;
 }
-void CQuest::setRequestedNumber(int num)
+void CQuest::setRequestedNumberOfItens(int num)
 {
-	_qtdNecessaria = num;
+	_qtdNecessariaItem = num;
+}
+void CQuest::setRequestedNumberOfEnemies(int num)
+{
+	_qtdNecessariaInimigo = num;
 }
 void CQuest::setReward(int num)
 {
@@ -100,15 +129,61 @@ void CQuest::setLoyaltyReward(CLealdade *num)
 	_recompensaLealdade = num;
 }
 //
-void CQuest::beginQuest(/*CPersonagem *personagem*/)
+bool CQuest::testEnemyKilled(CPersonagem *personagem)
 {
+	if(personagem->getType() == _inimigoNecessario)
+	{
+		_monstrosDerrotados = _monstrosDerrotados + 1;
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
+}
+bool CQuest::testItemPicked(CItem *item)
+{
+	if(item->getTipo() == _itemNecessario)
+	{
+		_itensPossuidos = _itensPossuidos + 1;
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
+}
+void CQuest::beginQuest(CPersonagemJogador *jogador, CPersonagem *npc, int itemID, int qtdItem, int monstID, int qtdMonst, int rewdXp, int rewdMny, CLealdade *rewdLdd, CItem *rewdItm)
+{
+	_estado = Q_OPEN;
+	_jogador = jogador;
+	_contratante = npc;
+	_itemNecessario = (TypeItens)itemID;
+	_inimigoNecessario = (TypeModelos)monstID;
+	_qtdNecessariaItem = qtdItem;
+	_qtdNecessariaInimigo = qtdMonst;
+	_monstrosDerrotados = 0;
+	_itensPossuidos = 0;
+	_recompensaItem = rewdItm;
+	_recompensaDinheiro = rewdMny;
+	_recompensaXP = rewdXp;
+	_recompensaLealdade = rewdLdd;
 }
 bool CQuest::tryFinish()
 {
-	return(true);
+	if((_itensPossuidos >= _qtdNecessariaItem)&&(_monstrosDerrotados >= _qtdNecessariaInimigo))
+	{
+		_estado = Q_FINISHED;
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
 }
 void CQuest::finishQuest()
 {
+	_estado = Q_CLOSED;
 }
 
 #endif
